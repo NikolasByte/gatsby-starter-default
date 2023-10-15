cd2bdf6c60f754aa0468676485f9ae0433628e9a
Nikolas Charalambous <nikolas.c@hotmail.co.uk>
2023-10-15 23:13:06 +0100

    added GA and testing helmet replace

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,6 +123,11 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => (
+  <>
+    <title>Home</title>
+    <meta name="description" content="Hello World" />
+  </>
+)
 
 export default IndexPage
